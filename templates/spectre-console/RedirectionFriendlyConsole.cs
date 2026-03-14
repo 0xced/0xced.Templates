65ded39e4c769d65adc60be83357f18de4a17910
Cédric Luthi <cedric.luthi@gmail.com>
2026-03-14 17:01:30 +0100

    Workaround Rider issue

--- templates/spectre-console/RedirectionFriendlyConsole.cs
+++ templates/spectre-console/RedirectionFriendlyConsole.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Threading;
 using Spectre.Console;
 
 namespace myapp;
@@ -23,7 +24,21 @@ public static class RedirectionFriendlyConsole
 
     private sealed class RedirectionFriendlyAnsiConsoleOutput(IAnsiConsoleOutput ansiConsoleOutput) : IAnsiConsoleOutput
     {
-        public TextWriter Writer => ansiConsoleOutput.Writer;
+        public TextWriter Writer
+        {
+            get
+            {
+                var count = 0;
+                while (ansiConsoleOutput.Width == 80 && Environment.GetEnvironmentVariable("__CFBundleIdentifier") == "com.jetbrains.rider" && count < 100)
+                {
+                    // Because of https://youtrack.jetbrains.com/issue/IJPL-112721/Terminal-width-is-applied-asynchronously-which-leads-to-inconsistent-line-breaking-on-Windows
+                    Thread.Sleep(millisecondsTimeout: 10);
+                    count++;
+                }
+                return ansiConsoleOutput.Writer;
+            }
+        }
+
         public bool IsTerminal => ansiConsoleOutput.IsTerminal;
         public int Width => IsTerminal ? ansiConsoleOutput.Width : 320;
         public int Height => IsTerminal ? ansiConsoleOutput.Height : 240;
